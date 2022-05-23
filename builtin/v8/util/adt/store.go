d400d329d9c151563de6478e087468a2181a77be
hunjixin <1084400399@qq.com>
2022-05-23 18:22:55 +0800

    remove refmt package

--- builtin/v8/util/adt/store.go
+++ builtin/v8/util/adt/store.go
@@ -3,11 +3,16 @@ package adt
 import (
 	"context"
 
-	ipldcbor "github.com/ipfs/go-ipld-cbor"
+	cid "github.com/ipfs/go-cid"
 )
 
 // Store defines an interface required to back the ADTs in this package.
 type Store interface {
 	Context() context.Context
-	ipldcbor.IpldStore
+	IpldStore
+}
+
+type IpldStore interface {
+	Get(ctx context.Context, c cid.Cid, out interface{}) error
+	Put(ctx context.Context, v interface{}) (cid.Cid, error)
 }
